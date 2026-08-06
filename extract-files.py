42af046d24119ff4cd2031a94b6cda0e74c54515
TTahaKeles <tanimazsinxxl@gmail.com>
2026-08-06 02:33:50 +0000

    import ximiparts

--- extract-files.py
+++ extract-files.py
@@ -22,6 +22,7 @@ namespace_imports = [
     'hardware/qcom-caf/sm8450',
     'hardware/qcom-caf/wlan',
     'hardware/xiaomi',
+    'packages/apps/XiaomiParts',
     'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/dataservices',
